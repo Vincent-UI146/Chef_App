ad72d7378d0b957161b2c98ba2cca6c5e622be5a
Vincent-UI146 <vincentkabo589@gmail.com>
2025-11-10 09:25:26 +0000

    types

--- types.ts
+++ types.ts
@@ -1,8 +1,61 @@
-import { Course } from './Index'; 
+export type Course = 'Starters' | 'Main Courses' | 'Desserts' | 'Drinks';
+
 export interface MenuItem {
   id: string;
   name: string;
-  price: number;
   description: string;
+  price: number;
   course: Course;
+  createdAt: Date;
+  isFeatured: boolean;
+}
+
+export interface FilterState {
+  courses: string[];
+  priceRange: {
+    min: number;
+    max: number;
+  };
+  featuredOnly: boolean;
+}
+
+export interface CourseStats {
+  course: Course;
+  count: number;
+  averagePrice: number;
+  totalPrice: number;
+  mostExpensive: number;
+  cheapest: number;
+}
+
+export interface MenuAnalysis {
+  totalItems: number;
+  totalValue: number;
+  courseStats: CourseStats[];
+  mostExpensiveItem: MenuItem | null;
+  cheapestItem: MenuItem | null;
+}
+
+export type RootStackParamList = {
+  Home: undefined;
+  ManageMenu: undefined;
+  Filter: {
+    filters: FilterState;
+    onApplyFilters: (filters: FilterState) => void;
+  };
+};
+
+// Global type for application constants
+export interface AppConstants {
+  MAX_PRICE: number;
+  MIN_PRICE: number;
+  MAX_NAME_LENGTH: number;
+  MAX_DESCRIPTION_LENGTH: number;
+  COURSES: Course[];
+}
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
 }
